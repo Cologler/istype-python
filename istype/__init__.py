2647d077a6dcee9a947c016bc5223cb269194393
Cologler <skyoflw@gmail.com>
2018-01-13 13:37:00 +0800

    add match

--- istype/__init__.py
+++ istype/__init__.py
@@ -6,4 +6,38 @@
 #
 # ----------
 
+from typing import Iterable
+from itertools import zip_longest
 from .internal.common import isinstanceof, issubclassof
+
+def match(args: (list, tuple), types: Iterable[type]) -> bool:
+    '''
+    check whether args match types.
+
+    example:
+    ``` py
+    `match(('', 1), (str, int)) # True
+    ```
+    '''
+
+    try:
+        if len(args) != len(types):
+            return False
+    except TypeError:
+        # object of type 'types' has no len()
+        pass
+
+    empty = object()
+    for item, typ in zip_longest(args, types, fillvalue=empty):
+        if item is empty or typ is empty:
+            return False
+        if not isinstanceof(item, typ):
+            return False
+    return True
+
+
+__all__ = [
+    'isinstanceof',
+    'issubclassof',
+    'match'
+]
